546b7c5c00ffe6af068f518ad6630570391a93ca
ranjitvaradi <ranjit.vnv@gmail.com>
2020-08-09 14:14:05 +0530

    Update provder.tf

--- provder.tf
+++ provder.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   version = "~> 3.0"
   region  = "us-east-1"
-  access_key = "AKIAXFSUJJ6DPAKHCVER"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
