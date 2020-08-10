ad5bb553b4ddc7440fed586f1de76c44d8c6b253
ranjit.vnv <ranjit.vnv@gmail.com>
2020-08-10 06:23:29 +0530

    null

--- provder.tf
+++ provder.tf
@@ -2,3 +2,12 @@ provider "aws" {
   version = "~> 3.0"
   region  = "us-east-1"
 }
+resource "aws_instance" "web" {
+  ami           = "ami-0ac80df6eff0e70b5"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
